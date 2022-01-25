ce0e322ce54d81729cb255e0294ad921609e5b67
Aetopia <kirbymario22@gmail.com>
2022-01-25 13:56:39 +0530

    Update render.py

--- scripts/render.py
+++ scripts/render.py
@@ -13,11 +13,10 @@ Temp=os.environ["TEMP"]
 def Render(VideoList, Option):
     Settings=ConfigRead(Option)
     for Video in list(VideoList):
-
+        
         Video=os.path.abspath(Video)
         VideoPath=os.path.split(Video)[0]
         VideoFile=os.path.split(Video)[1]
-        Output=f"{VideoPath}\\{Prefix}_{VideoFile}"
         Arguments=Settings[1]  
 
         if Option == "resample":
@@ -25,7 +24,10 @@ def Render(VideoList, Option):
             Prefix="Resampled"
         elif Option == "interpolate":
             Interpolate(Video,Settings[2],Settings[3],Settings[4],Settings[5],Settings[6],Settings[7]) 
-            Prefix="Interpolated"     
+            Prefix="Interpolated" 
+
+        Output=f"{VideoPath}\\{Prefix}_{VideoFile}"
+
         if os.path.isfile(Output):
             os.remove(Output)
         # Variables
@@ -41,7 +43,7 @@ def Render(VideoList, Option):
         if Arguments=="":
             Arguments=[]
         else:
-            Arguments=Arguments.split(" ")    
+            Arguments=Settings[1].split(" ")    
         Main=Pipe+[Settings[0]]+Input+Arguments+Output
         print(f"Video: {Video}\n")      
         subprocess.run(Main,shell=True) 
