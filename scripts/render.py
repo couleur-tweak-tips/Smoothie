ee0a7063864aaa6a6fc5dcc8d2860d6cc29dacee
Aetopia <kirbymario22@gmail.com>
2022-01-25 13:47:39 +0530

    Add av1an pipe support & remove HW Accel Args.
    Added partial av1an support into Smoothie.
    
    Removal of HW Accel Args.
    
    Option to either use ffmpeg or av1an in the config file.

--- scripts/render.py
+++ scripts/render.py
@@ -13,27 +13,36 @@ Temp=os.environ["TEMP"]
 def Render(VideoList, Option):
     Settings=ConfigRead(Option)
     for Video in list(VideoList):
+
         Video=os.path.abspath(Video)
         VideoPath=os.path.split(Video)[0]
         VideoFile=os.path.split(Video)[1]
+        Output=f"{VideoPath}\\{Prefix}_{VideoFile}"
+        Arguments=Settings[1]  
 
         if Option == "resample":
             Resample(Video,Settings[2],Settings[3])
             Prefix="Resampled"
         elif Option == "interpolate":
             Interpolate(Video,Settings[2],Settings[3],Settings[4],Settings[5],Settings[6],Settings[7]) 
-            Prefix="Interpolated" 
-        if os.path.isfile(f"{VideoPath}\\{Prefix}_{VideoFile}"):
-            os.remove(f"{VideoPath}\\{Prefix}_{VideoFile}")
+            Prefix="Interpolated"     
+        if os.path.isfile(Output):
+            os.remove(Output)
         # Variables
         if "ffmpeg.exe" in os.path.split(Settings[0])[1]:
-            InFile="-"
-            Pipe=['vspipe', '-y', Temp+'/Render.vpy','-','|']
-        else:
-            InFile=Temp+'/Render.vpy' 
+            Input=['-loglevel', 'error', '-hide_banner', '-stats', '-i', '-']
+            Pipe=['vspipe', '-y', Temp+'\\Render.vpy','-','|']
+            Output=[Output]
+        elif "av1an.exe" in os.path.split(Settings[0])[1]:
+            Input=['-i', f'{Temp}\\Render.vpy']
             Pipe=[]
-        OutFile=f"{VideoPath}\\{Prefix}_{VideoFile}"
-        Main=Pipe+[Settings[0]]+findall(r'[A-Z]+:(?:\\[^\\]+)+\.\w+|\S+',f"{Settings[1]}".format(Input=InFile,Output=OutFile)) 
+            Output=['-o', Output]
+  
+        if Arguments=="":
+            Arguments=[]
+        else:
+            Arguments=Arguments.split(" ")    
+        Main=Pipe+[Settings[0]]+Input+Arguments+Output
         print(f"Video: {Video}\n")      
         subprocess.run(Main,shell=True) 
         print("")
