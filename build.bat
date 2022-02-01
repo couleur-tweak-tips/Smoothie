bc14621335b89793173f9ee348ec6f77347683f2
Aetopia <kirbymario22@gmail.com>
2022-02-01 18:48:58 +0530

    Reduced Source Code Footprint aka made Minimal.

--- build.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-py -OO -m nuitka --onefile --follow-imports --include-plugin-files=Modules\config.py --include-plugin-files=Modules\encoder.py --include-plugin-files=Modules\vspipe.py --include-plugin-files=Modules\render.py main.py --output-dir="%TEMP%" -o "Smoothie.exe"
-timeout 5
--- main.py
+++ main.py
@@ -1,37 +1,49 @@
-import argparse
-import sys
-import subprocess
-from os import environ
-sys.path.insert(0, 'modules')
-from config import ConfigExist
-from render import Render
+# Modules
+from argparse import ArgumentParser
+from pathlib import Path
+import platform
+from os import path, system
+from sys import argv
+from subprocess import run
+from configparser import ConfigParser
 
-Folder=f'{environ["APPDATA"]}/Smoothie'
-
-ConfigExist()
-
-Parser=argparse.ArgumentParser(prog="Smoothie",
-usage="""
-smoothie -resample Video1.mp4 Video2.mp4 Video3.mp4
-smoothie -interpolate Video1.mp4 Video2.mp4 Video3.mp4""")
-
-Parser.add_argument('-resample',action="store",
-help="Resample single or multiple videos at once.",
-nargs='*',
-metavar=("<Videos>"))
-
-Parser.add_argument('-interpolate',action="store",
-help="Interpolate and resample single or multiple videos at once.",
-nargs='*',
-metavar=("<Videos>"))
-
-Parser.add_argument('-edit',action="store_true",help="Edit Smoothie's Config file.")
-
+# CLI Arguments
+Parser=ArgumentParser(usage="",add_help=False)
+Parser.add_argument('-resample', '-r', action="store", nargs="*")
+Parser.add_argument('-interpolate', '-i', action="store", nargs="*")
 Arguments=Parser.parse_args()
 
+# Functions
+def Render(Videos, Recipe=f"{path.abspath(path.split(argv[0])[0])}/settings/recipe.ini", Interpolate=False, Prefix="Resampled"):
+    Program=ConfigParser()
+    Program.read(Recipe)
+    Queue=len(Videos)
+    for Video in Videos:
+        Queue-=1
+        if Queue != 0:
+            if platform.system() == "Windows":
+                system(f"title Smoothie - Videos Queued: {Queue}")
+            else:
+                print(f"Videos Queued: {Queue}\n")    
+        else:
+            if platform.system() == "Windows":
+                system(f"title Smoothie")                       
+        VideoPath, VideoFile=path.split(path.abspath(Video))[0],path.split(path.abspath(Video))[1]
+        Command=f'vspipe -c y4m -a Input="{path.abspath(Video)}" -a Interpolate="{Interpolate}" -a Config="{Recipe}" "{path.abspath("Vapoursynth.vpy")}" - | {Program["rendering"]["process"]} -y -loglevel error -hide_banner -stats -i - {Program["rendering"]["arguments"]} "{VideoPath}/{Prefix} {VideoFile}"'
+        print(f"Video: {Video}\n")
+        run(Command,shell=True)
+        print("")
+
 if Arguments.resample is not None:
-    Render(Arguments.resample, "resample")
+    if Path(Arguments.resample[0]).suffix == '.ini':
+        Render(Arguments.resample[1::], Recipe=path.abspath(Arguments.resample[0]))
+    else:
+        Render(Arguments.resample)    
+
 elif Arguments.interpolate is not None:
-    Render(Arguments.interpolate, "interpolate")
-elif Arguments.edit is True:
-    subprocess.Popen(['start',f'{Folder}/Smoothie-Config.ini'],shell=True)     
+    if Path(Arguments.interpolate[0]).suffix == '.ini':
+        Render(Arguments.interpolate[1::], 
+        Recipe=path.abspath(Arguments.interpolate[0]), 
+        Interpolate=True,Prefix="Interpolated")
+    else:
+        Render(Arguments.interpolate, Interpolate=True)         
