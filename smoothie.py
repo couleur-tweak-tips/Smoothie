9da0482135045729a180a74efde2f331b457ab13
Aetopia <kirbymario22@gmail.com>
2022-02-02 13:57:58 +0530

    Added Error Outputting + Imports on needed Classes + Functions.

--- smoothie.py
+++ smoothie.py
@@ -1,10 +1,10 @@
 # Modules
 from argparse import ArgumentParser
 from pathlib import Path
-import platform
-from os import path, system
-import sys
-import subprocess
+from platform import system
+from os import path
+from sys import argv
+from subprocess import Popen, PIPE, STDOUT
 from configparser import ConfigParser
 
 # CLI Arguments
@@ -14,7 +14,7 @@ Parser.add_argument('-interpolate', '-i', action="store", nargs="*")
 Arguments=Parser.parse_args()
 
 # Functions
-def Render(Videos, Recipe=f"{path.abspath(path.split(sys.argv[0])[0])}/settings/recipe.ini", Interpolate=False, Prefix="Resampled"):
+def Render(Videos, Recipe=f"{path.abspath(path.split(argv[0])[0])}/settings/recipe.ini", Interpolate=False, Prefix="Resampled"):
     Config=ConfigParser()
     Config.read(Recipe)
     Queue=len(Videos)
@@ -22,32 +22,41 @@ def Render(Videos, Recipe=f"{path.abspath(path.split(sys.argv[0])[0])}/settings/
 
         Queue-=1
         if Queue != 0:
-            if platform.system() == "Windows":
+            if system() == "Windows":
                 import ctypes
                 ctypes.windll.kernel32.SetConsoleTitleW(f"Smoothie - Videos Queued: {Queue} | Rendering: {path.split(Video)[1]}")
             else:
                 print(f"Videos Queued: {Queue}")    
         else:
-            if platform.system() == "Windows":
+            if system() == "Windows":
                 import ctypes
                 ctypes.windll.kernel32.SetConsoleTitleW(f"Smoothie - Rendering: {path.split(Video)[1]}")    
 
         VideoPath, VideoFile = path.split(path.abspath(Video))[0],path.split(path.abspath(Video))[1]
-        VSPipe = f'vspipe -c y4m -a Input="{path.abspath(Video)}" -a Interpolate="{Interpolate}" -a Config="{Recipe}" "{path.abspath(path.split(sys.argv[0])[0])}/vs.vpy" -' 
+        VSPipe = f'vspipe -c y4m -a Input="{path.abspath(Video)}" -a Interpolate="{Interpolate}" -a Config="{Recipe}" "{path.abspath(path.split(argv[0])[0])}/vs.vpy" -' 
         FFmpeg = f'ffmpeg -y -loglevel error -hide_banner -stats -i - {Config["rendering"]["arguments"]} "{VideoPath}/{Prefix} - {VideoFile}"'
         Command=f'{VSPipe} | {FFmpeg}'
         print(f"\n> Video: {path.split(Video)[1]}")
-        Process = subprocess.Popen(Command, shell=True, encoding='utf-8', stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        Process = Popen(Command, shell=True, encoding='utf-8', stdout=PIPE, stderr=STDOUT)
+
+        # Output
+        FullOutput=""
         while True:
             Output = Process.stdout.readline()
+            FullOutput+=Output
             if Output == '' or Process.poll() is not None:
                 break
             if Output:
                 try:
                     print(f"> Time: {Output.strip().split('=')[5].split(' ')[0]} | Speed: x{Output.strip().split('=')[7].split(' ')[0].replace('x','')}", end='\r',flush=True)
                 except:
-                    break      
-        print("")
+                    pass 
+        print("")           
+        Process.communicate()
+        if Process.returncode == 1:
+            print(FullOutput)
+            exit()
+
 
 # Arguments Parsing
 if Arguments.resample is not None:
