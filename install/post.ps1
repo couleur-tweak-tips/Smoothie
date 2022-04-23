4d2b0cc3b123bd90ceeecc07e34f21f3ad5f64ba
Couleur <82747632+couleurm@users.noreply.github.com>
2022-04-23 21:17:42 +0200

    Use TweakList's encoding test

--- install/post.ps1
+++ install/post.ps1
@@ -9,29 +9,9 @@ if (-Not(Test-Path "$ScoopDir\shims\sm.shim")){
     Add-Content "$ScoopDir\shims\sm.shim" "path = `"$DIR\VapourSynth\python.exe`"$([Environment]::NewLine)args = `"$DIR\Smoothie\Smoothie.py`""
 }
 
-Write-Warning "Testing FFmpeg encoders.. note NVENC may fail if you use an older driver / older FFmpeg version"
-@(
-    'hevc_nvenc -rc constqp -preset p7 -qp 18'
-    'h264_nvenc -rc constqp -preset p7 -qp 15'
-    'hevc_amf -quality quality -qp_i 16 -qp_p 18 -qp_b 20'
-    'h264_amf -quality quality -qp_i 12 -qp_p 12 -qp_b 12'
-    'hevc_qsv -preset veryslow -global_quality:v 18'
-    'h264_qsv -preset veryslow -global_quality:v 15'
-    'libx265 -preset medium -crf 18'
-    'libx264 -preset slow -crf 15'
-) | ForEach-Object -Begin {
-    $script:shouldStop = $false
-} -Process {
-    if ($shouldStop -eq $true) { return }
-    Invoke-Expression "ffmpeg.exe -loglevel fatal -f lavfi -i nullsrc=3840x2160 -t 0.1 -c:v $_ -f null NUL"
-    if ($LASTEXITCODE -eq 0){
-        $script:valid_args = $_
-        $shouldStop = $true # Crappy way to stop the loop since most people that'll execute this will technically be parsing the raw URL as a scriptblock
+Invoke-RestMethod https://tl.ctt.cx | Invoke-Expression
 
-    }
-}
-
-$rc = (Get-Content "$DIR\Smoothie\settings\recipe.ini") -replace ('libx264 -preset slow -crf 15',$valid_args)
+$rc = (Get-Content "$DIR\Smoothie\settings\recipe.ini") -replace ('libx264 -preset slow -crf 15',(Get-EncodingArgs))
 
 if ($valid_args -like "*libx26*"){
     $rc = $rc -replace ('gpu=true','gpu=false')
