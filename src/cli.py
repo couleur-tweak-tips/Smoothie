4a34803a7d4d5cac49d69af97cdf98a4b357c917
Couleur <82747632+couleurm@users.noreply.github.com>
2022-11-23 18:40:33 +0100

    add -!! and -stripaudio

--- src/cli.py
+++ src/cli.py
@@ -1,16 +1,18 @@
 from argparse import ArgumentParser
+from constants import SRCDIR
+from os import path
+from sys import argv
 parser = ArgumentParser()
 add_arg = parser.add_argument
-# i/o
 
-add_arg("-input",   "-i",   
+add_arg("-input", "-i",   
         help=" specify input video path(s)",
         action="store",
         nargs="+",
         metavar='PATH',
         type=str)
 
-add_arg("-output",  "-o",
+add_arg("-output", "-o",
         help=" specify output video path(s)",
         action="store",
         nargs="+",
@@ -22,19 +24,23 @@ add_arg("-dir",
         help=" opens the directory where Smoothie resides",
         action="store_true")
 
-add_arg("-recipe",  "-rc",
+add_arg("-recipe", "-rc",
         help=" opens default recipe.ini",
         action="store_true")
  
 # overrides/behavior 
-add_arg("-peek",    "-p",
+add_arg("-peek", "-p",
         help=" Render a specific frame (outputs an image)",
         action="store",
         nargs=1,
         metavar='752',
         type=int)
 
-add_arg("-config",  "-c",
+add_arg("-stripaudio", "-an",
+        help=" Doesn't output audio track",
+        action="store_true")
+
+add_arg("-config", "-c",
         help=" specify override config file",
         action="store",
         nargs=1, 
@@ -57,17 +63,17 @@ add_arg("-verbose", "-v",
         help=" increase output verbosity",
         action="store_true")
 
-add_arg("-veryverbose","-vv",
+add_arg("-veryverbose", "-vv",
         help=" increase output verbosity",
         action="store_true")
 
-add_arg("-outdir",  "-outd",
+add_arg("-outdir", "-outd",
         help=" save all output to current directory",
         nargs='?',
         const='')
 
 add_arg("-vpy",
-        help=" specify a VapourSynth script",
+        help=" use another VS script",
         action="store",
         nargs=1,
         metavar='PATH',
@@ -111,4 +117,19 @@ add_arg("-split",
         help=" Split your cut segments into multiple files",
         action="store_true")
 
-args = parser.parse_args()
+add_arg("-rerun", '-!!',
+        help=" Split your cut segments into multiple files",
+        action="store_true")
+
+lastargs = path.join(SRCDIR, 'lastargs.txt')
+
+with open(lastargs, "r+", encoding="UTF-8") as f:
+        if len(argv) == 2 and argv[1] in ["-!!", "-rerun"]:
+                args = parser.parse_args(f.read().splitlines())
+                
+        else:
+                f.truncate(0)
+                f.write("\n".join(argv[1:]))
+                args = parser.parse_args()
+
+
