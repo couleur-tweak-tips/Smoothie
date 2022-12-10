41ca0874ab65373a92f666fa0c448d91f64f181c
Couleur <82747632+couleurm@users.noreply.github.com>
2022-12-10 22:48:48 +0100

    reformat progress bar, with help from @Aetopia

--- src/bar.py
+++ src/bar.py
@@ -1,7 +1,7 @@
 import subprocess
-from subprocess import Popen
+from fnmatch import fnmatch
 from os import get_terminal_size, path
-
+from time import sleep
 from helpers import *
 import colors
 import constants
@@ -9,22 +9,11 @@ import constants
 if constants.ISWIN:
     import PyTaskbar
 
-    # if 'duration' in stream:
-    #     length = stream['duration']
-    # elif 'DURATION' in stream['tags']:
-    #     length = get_sec(stream['tags']['DURATION'])
-    # else:
-    #     raise Exception('No duration found in video metadata')
-    # return round(float(length))
-
 def Bar (cmd: dict):
     try:
-        # vs_proc = sp.Popen(cmd['ff'], stdout=sp.PIPE, stderr=sp.PIPE)
-        # ff_proc = sp.Popen(cmd['vs'], stdin=vs_proc.stdout, stdout=sp.PIPE, stderr=sp.PIPE)
-        process = Popen(
+        process = subprocess.Popen(
             (cmd['vs'] + '|' + cmd['ff']),
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
+            stderr=subprocess.PIPE,
             universal_newlines=True,
             shell=True
             )
@@ -45,96 +34,85 @@ def Bar (cmd: dict):
                 prog.init()
                 prog.setState('loading')
                 
-        spinner = yaspin(
-            Spinner(['\\', '|', '/', '-'], 50),  # type: ignore
-            text=spinnertext)
+                spinner = yaspin(
+                    Spinner(['\\', '|', '/', '-'], 50),  # type: ignore
+                    text=spinnertext)
 
-        spinner.start()
-        stats = {}
+        spinner.start()        
         duration = probe(cmd["path"])['duration']
-        vid_length = round(float(duration))
-        First = False
-        log = []
-        for current in process.stdout:
-            log.append(current)
-            if not First:
-                spinner.stop()
-                First = True
+        vid_length = float(duration)
+        
+        def display_bar (current: str) -> str:
+            columns = get_terminal_size()[0]
+            barsize = columns - (52 + len(path.basename(cmd["path"])))
+
+            stats = {}
+            statistic = current.replace('  ',' ').replace('  ',' ').replace('= ','=').replace('= ','=').split(' ')
+            for stat in statistic:
+                if '=' in stat:
+                    key, val = stat.split('=')
+                    # print(key, val)
+                    stats[key] = val
+ 
+            if 'time' not in stats.keys():
+                return "Bar: FFmpeg output does not contain time value yet"
+            elif stats['time'].startswith("-"): # negative time happens at first sometimes, can be disturbing
+                return ""
             
-            if current != '' or not current.startswith("Script"):
-                statistic = current.replace('  ',' ').replace('  ',' ').replace('= ','=').replace('= ','=').split(' ')
-                for stat in statistic:
-                    if '=' in stat:
-                        key, val = stat.split('=')
-                            
-                        stats[key] = val
+            secs_rendered = get_sec(stats['time'])
+            percentage = round((secs_rendered*100) / vid_length, 1)
+            progress = round(((percentage / 100) * barsize)) # per*size of colums*tage
 
-                #if stats == {}: # If it failed to parse
-                #    print(process.stderr)
-                #    raise Exception(f'\n\nVapourSynth failed to process the video, \n\nRun this in cmd for more info: sm -i "{path.abspath(video)}" -v ')
+            if 'x' in stats['speed']:
+                stats['speed'] = float(stats['speed'].strip('x'))
+                if stats['speed'] >= 100:
+                    stats['speed'] = 99.99
+                stats['speed'] = str(round((stats['speed']), 2)) + 'x'
 
-                if 'time' not in stats.keys(): continue
-                secs_rendered = get_sec(stats['time'])
-                percentage = round((secs_rendered*100) / vid_length, 1)
+            if isWT:
+                setWTprogress(percentage)
+                bar = '\033[38;5;83m\033[9m' + (" " * progress)
+                bar += '\033[38;5;241m' + (" " * (barsize - progress)) + '\033[29m\033[0m'
+            else:
+                prog.setProgress(int(percentage))
+                bar = '\033[38;5;83m' + ("━" * progress)
+                bar += '\033[38;5;241m' + ("─" * (barsize - progress)) + '\033[0m'  
+            if (percentage <= 10): bar = ' ' + bar
+                        
+            w = "\033[38;5;255m"
+            g = "\033[38;5;245m"
+            return str(
+f"\033[u\033[0J\033[?25l\
+{w}{path.basename(cmd['path'])} {g}| \
+{w}time{g}: {stats['time']} \033[38;5;245m| \
+{w}speed{g}: {stats['speed']} \033[38;5;245m| \
+{w}{percentage}\033[38;5;87m% \033[38;5;245m|\033[0m \
+{bar}")
+            #return str(f"{secs_rendered}*100/{vid_length}={percentage}")
 
-                columns = get_terminal_size()[0]
-                barsize = columns - (52 + len(path.basename(cmd["path"])))
-                progress = round(((percentage / 100) * barsize))
-
-                #━ ╸
-                # If user is running Windows Terminal, use strikthrough
-                # Else use ─ (not -) for progress bar
-                # They both look the same, but are not compatible with eachanother
+        log = []
+        for line in iter(process.stderr.readline, ""):
+            current = line.strip()
+            spinner.stop()
+            indexText = "Creating lwi index file "
+            if current.startswith(indexText):
+                message = "Indexing: " + current.strip(indexText)
+            elif fnmatch(current, "frame=*fps=*q=*size=*time=*bitrate=*speed=*"):
+                message = display_bar(current)
+            elif fnmatch(current, "Output * frames in * seconds (* fps)"):
+                break
+            else:
+                # I doubledog dare you find a better way in Python
+                # to both error handle and format stdout like I do
+                message = current
+                log.append(current)
                 
-                if percentage > 100: percentage = 100
-                
-                if constants.ISWIN:
-                    if isWT:
-                        setWTprogress(percentage)
-                        sep = '｜'
-                        bar = '\033[38;5;83m\033[9m' + (" " * progress)
-                        bar += '\033[38;5;241m' + (" " * (barsize - progress)) + '\033[29m\033[0m'
-                    else:
-                        prog.setProgress(int(percentage))
-                        sep = '|'
-                        bar = '\033[38;5;83m' + ("━" * progress)
-                        bar += '\033[38;5;241m' + ("─" * (barsize - progress)) + '\033[0m'  
-                    
-                #if (percentage <= 10): # needs a space before it grows a char when it passes 10
-                #    pad = ' ' 
-                #else:
-                #    pad = ''
-                
-                if 'x' in stats['speed']:
-                    stats['speed'] = float(stats['speed'].strip('x'))
-                    if stats['speed'] >= 100:
-                        stats['speed'] = 99.99
-                    stats['speed'] = str(round((stats['speed']), 2)) + 'x'
-                    
-                if len(stats['speed']) == 4:
-                    stats['speed'] = stats['speed'] + ' ' # Balances if it shrinks from 1.21x to 1.2x (1 less char)
-                
-                w = "\033[38;5;255m"
-                g = "\033[38;5;245m"
-                print(
-f"\033[0J\033[?25l\
-{w}{path.basename(cmd['path'])} {g}{sep} \
-{w}time{g}: {stats['time']} \033[38;5;245m{sep} \
-{bar} \033[38;5;245m{sep} \
-{w}speed{g}: {stats['speed']} \033[38;5;245m{sep} \
-{w}{percentage}\033[38;5;87m%\033[0m", end='\r')
-                #print(f"\033[u\033[0J{percentage}", end='\r')
-                if percentage >= 100: break
-        if constants.ISWIN:
-            if isWT==True: setWTprogress(0) # Reset
-            else: prog.setProgress(0);prog.setState('normal');prog.setState('done')
-        
-        process.communicate()
-        if process.returncode != 0:
+            print(message, end="\r")
+            sleep(0.15)
+            if len(current) == 0: # if be finished
+                break
+        if log:
             return log
-        else:
-            return None
-        
     except KeyboardInterrupt:
         process.kill()
         spinner.stop()
